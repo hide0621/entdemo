887229e91f4bf9ccb3bb24920e6cc2828a230282
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-17 13:35:02 +0900

    Add Your First Edge (Relation)まで

--- ent/ent.go
+++ ent/ent.go
@@ -4,6 +4,8 @@ package ent
 
 import (
 	"context"
+	"entdemo/ent/car"
+	"entdemo/ent/group"
 	"entdemo/ent/user"
 	"errors"
 	"fmt"
@@ -65,7 +67,9 @@ type OrderFunc func(*sql.Selector)
 // columnChecker returns a function indicates if the column exists in the given column.
 func columnChecker(table string) func(string) error {
 	checks := map[string]func(string) bool{
-		user.Table: user.ValidColumn,
+		car.Table:   car.ValidColumn,
+		group.Table: group.ValidColumn,
+		user.Table:  user.ValidColumn,
 	}
 	check, ok := checks[table]
 	if !ok {
--- start.go
+++ start.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"entdemo/ent"
+	"entdemo/ent/car"
 	"entdemo/ent/user"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -34,4 +36,60 @@ func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 	}
 	log.Println("user returned: ", u)
 	return u, nil
+}
+
+func CreateCars(ctx context.Context, client *ent.Client) (*ent.User, error) {
+
+	// Create a new car with model "Tesla".
+	tesla, err := client.Car.
+		Create().
+		SetModel("Tesla").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", tesla)
+
+	// Create a new car with model "Ford".
+	ford, err := client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", ford)
+
+	// Create a new user, and add it the 2 cars.
+	a8m, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		AddCars(tesla, ford).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", a8m)
+	return a8m, nil
+}
+
+func QueryCars(ctx context.Context, a8m *ent.User) error {
+
+	//a8mユーザーが持っている全ての車をselectする
+	cars, err := a8m.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+	log.Println("returned cars:", cars)
+
+	//a8mユーザーが持っている車のうち、WhereでFordを条件付けしてselectする
+	ford, err := a8m.QueryCars().Where(car.Model("Ford")).Only(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+	log.Println(ford)
+	return nil
 }
